d5a014e01ea20adbf8cc18fb53e8e4c3edac1d13
fricosystem <fricoalimentossystem@gmail.com>
2025-10-26 19:25:11 -0300

    Transferência inteligente de fricosystem/rentguarantee-haven-57033-68851-72907-87580 - 26/10/2025 às 19:24 - src/pages/Login.tsx - 26/10/2025 19:24:56

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,22 +6,18 @@ import { ModulosSection } from "@/components/Login/ModulosSection";
 import { Testimonials } from "@/components/Login/Testimonials";
 import { Contact } from "@/components/Login/Contact";
 import { Footer } from "@/components/Login/Footer";
-
 const Login = () => {
-  return (
-    <div className="min-h-screen w-screen max-w-full overflow-x-hidden bg-green-100 dark:bg-gray-900/90 text-white flex flex-col">
+  return <div className="min-h-screen w-screen max-w-full overflow-x-hidden bg-green-100 dark:bg-gray-900/90 text-white flex flex-col">
       <Header />
       <main className="flex-grow w-full">
         <Hero />
-        <Features />
-        <Solutions />
+        
+        
         <ModulosSection />
-        <Testimonials />
+        
         <Contact />
       </main>
       <Footer />
-    </div>
-  );
+    </div>;
 };
-
 export default Login;
